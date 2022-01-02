4ccdbb0e206e97a5935af45e45300cb284247273
Ahmet F. Turhan <63173231+ahmetfturhan@users.noreply.github.com>
2022-01-02 15:40:29 +0300

    Add files via upload

--- admin.php
+++ admin.php
@@ -6,16 +6,22 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+  <script>
+    $(document).ready(function() {
+      $("#button1").click(function() {
+        $("#div1").load("add.html");
+      });
+    });
+  </script>
 </head>
 
 <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
@@ -381,62 +387,25 @@
   </div>
   <div class="container">
     <div class="row">
-
-      <div class="container rounded p-4 text-center">
-        <h2>Add New Item</h2>
-        <form action="addItem.php" method="post">
-          <div class="row justify-content-center">
-            <div class="col-4">
-              <select class="form-select mb-4" aria-label="Default select example" name="itemName">
-                <option selected>Select the item you want to add</option>
-                <option value="cpu" name="cpu">CPU</option>
-                <option value="gpu" name="gpu">GPU</option>
-                <option value="ram" name="ram">RAM</option>
-                <option value="storage" name="storage">Storage</option>
-                <option value="display" name="display">Display</option>
-                <option value="os" name="os">OS</option>
-              </select>
-            </div>
-          </div>
-
-          <div class="row justify-content-center">
-            <div class="form-floating mb-3 col-4">
-              <input type="text" class="form-control" id="cName" placeholder="Component Name" name="ComponentName">
-              <label for="cName">Component Name</label>
-            </div>
-
-            <div class="form-floating col-4">
-              <input type="text" class="form-control" id="price" placeholder="Price" name="price">
-              <label for="price">Price</label>
-            </div>
-          </div>
-          <div class="row justify-content-center">
-            <div class="form-floating mb-3 col-4">
-              <input type="text" class="form-control" id="manf" placeholder="Manufacturer ID" name="mid">
-              <label for="manf">ManufacturerID</label>
-            </div>
-
-            <div class="form-floating col-4">
-              <input type="text" class="form-control" id="stock" placeholder="Stock" name="stock">
-              <label for="stock">Stock</label>
-            </div>
-
-            <div class="form-floating col-4">
-              <input type="number" class="form-control" id="attr" placeholder="Attribute" name="attr">
-              <label for="attr">Attribute</label>
-            </div>
-          </div>
-          <button class="btn btn-lg btn-outline-black" type="submit">Add Item</button>
-        </form>
-      </div>
+      <form id="form1" method="post">
+        <select class="form-select mb-4" aria-label="select" name="operation">
+          <option selected>Select the operation</option>
+          <option value="add" name="add">ADD</option>
+          <option value="update" name="update">UPDATE</option>
+          <option value="delete" name="delete">DELETE</option>
+        </select>
+        <button class="btn btn-lg btn-outline-black" type="button" id="button1">Select</button>
+      </form>
 
     </div>
+    <div id="div1">
+          
+    </div>
+    
   </div>
 
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
 </html>
