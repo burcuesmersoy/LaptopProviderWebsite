53cf11d37b13ee24c62da1ccd99e47d71a9b149d
Ahmet F. Turhan <afaturhan@gmail.com>
2022-01-02 20:38:15 +0300

    redirect to admin.php

--- updateItem.php
+++ updateItem.php
@@ -16,29 +16,34 @@ $price = $_POST['price'];
 $itemName = $_POST['itemName'];
 $id = $_POST['id'];
 
-echo $stock;
-echo $price;
-echo $itemName;
-echo $id;
-
 $itemID = $itemName;
 $itemID .= "ID";
 
-if($stock!="default" && $price=="default"){
+if ($stock != "default" && $price == "default") {
     $sql = "UPDATE $itemName SET `stock`='$stock' WHERE $itemID='$id'";
     $conn->query($sql);
-}
-elseif ($stock=="default" && $price!="default") {
+    if ($conn->query($sql) === TRUE) {
+        header("Location: admin.php");
+        exit();
+    }
+} elseif ($stock == "default" && $price != "default") {
     $sql = "UPDATE $itemName SET `price`='$price' WHERE $itemID='$id'";
     $conn->query($sql);
-
-}
-elseif ($stock!="default" && $price!="default") {
+    if ($conn->query($sql) === TRUE) {
+        header("Location: admin.php");
+        exit();
+    }
+} elseif ($stock != "default" && $price != "default") {
     $sql1 = "UPDATE $itemName SET `price`='$price' WHERE $itemID='$id'";
     $sql2 = "UPDATE $itemName SET `stock`='$stock' WHERE $itemID='$id'";
     $conn->query($sql1);
     $conn->query($sql2);
-}
-else {
-    echo 'yazacagin kodu';
+
+    if ($conn->query($sql1) === TRUE && $conn->query($sql2) === TRUE) {
+        header("Location: admin.php");
+        exit();
+    }
+} else {
+    echo 'an error has been occured';
+    exit();
 }
