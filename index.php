16968e6b78f0077872b8e118430ac7195747b3f7
Kakule <a.brkkuscu@gmail.com>
2022-01-02 21:41:07 +0300

    Her seçim sonrası price hesaplama index.php için.

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ $dbname = "sigma";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+
 if ($conn->connect_error) {
     die("connection failed: " . $conn->connect_error);
 } else {
