baa1e65e078e812747b9a407fb3cda35966d8617
yilmazasl <61515099+yilmazasl@users.noreply.github.com>
2022-01-03 17:10:08 +0300

    Add files via upload
    navbarını düzelttim

--- register.php
+++ register.php
@@ -25,8 +25,14 @@ if (isset($_SESSION['email']))
     <body class="bg-dark">
         <section id="nav-bar">
             <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
+                <div class="container-fluid"><a class="navbar-brand" href="">
+                    <img src="https://cdn-icons-png.flaticon.com/512/1001/1001266.png" style="width: 40px;" alt="">JoinSigma Inc</a>
+                
+    
+                <br>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
                         aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
