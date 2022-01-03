1946457e3043e78e4240a224421ba0468b93841b
Ahmet F. Turhan <afaturhan@gmail.com>
2022-01-03 20:54:22 +0300

    bilimom

--- admin.php
+++ admin.php
@@ -34,7 +34,7 @@ if (!($_SESSION['email'] == 'admin@gmail.com'))
 <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="index.php">Sigma</a>
+      <a class="navbar-brand" href="loggedindex.php">Sigma</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
